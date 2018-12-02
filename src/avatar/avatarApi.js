237c234f4f8d76e03928c1960b67eacc86e00490
Madis Jullinen <mjullinen@twilio.com>
2018-12-02 11:26:24 +0200

    testing

--- src/avatar/avatarApi.js
+++ src/avatar/avatarApi.js
@@ -1,6 +1,6 @@
 // @flow
 
-const tokenApi: string = process.ENV === 'production' ? 'fixme' : 'http://localhost:4000';
+const tokenApi: string = process.ENV === 'production' ? 'https://blackford.xyz' : 'http://localhost:4000';
 
 export const getDeckerStats = (authentication: string): Promise<any> => fetch(`${tokenApi}/api/v1/decker/character`, {
   headers: {
--- src/token/tokenApi.js
+++ src/token/tokenApi.js
@@ -1,26 +1,24 @@
 // @flow
 import type { AuthPayload, TokenApiResponse } from '../types/General';
 
-const tokenApi: string = process.ENV === 'production' ? 'fixme' : 'http://localhost:4000';
+const tokenApi: string = process.ENV === 'production' ? 'https://blackford.xyz' : 'http://localhost:4000';
 
-export const postTokenApi = (payload: AuthPayload): Promise<TokenApiResponse> =>
-  fetch(`${tokenApi}/token`, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      Accept: 'application/json',
-      'Content-Length': Object.keys(payload).length.toString(),
-      'Content-Type': 'application/json',
-    },
-  }).then(response => response.json());
+export const postTokenApi = (payload: AuthPayload): Promise<TokenApiResponse> => fetch(`${tokenApi}/api/token`, {
+  method: 'POST',
+  body: JSON.stringify(payload),
+  headers: {
+    Accept: 'application/json',
+    'Content-Length': Object.keys(payload).length.toString(),
+    'Content-Type': 'application/json',
+  },
+}).then(response => response.json());
 
-export const putTokenApi = (payload: {apiToken: string}): Promise<TokenApiResponse> =>
-  fetch(`${tokenApi}/token`, {
-    method: 'PUT',
-    body: JSON.stringify(payload),
-    headers: {
-      Accept: 'application/json',
-      'Content-Length': Object.keys(payload).length.toString(),
-      'Content-Type': 'application/json',
-    },
-  }).then(response => response.json());
+export const putTokenApi = (payload: {apiToken: string}): Promise<TokenApiResponse> => fetch(`${tokenApi}/api/token`, {
+  method: 'PUT',
+  body: JSON.stringify(payload),
+  headers: {
+    Accept: 'application/json',
+    'Content-Length': Object.keys(payload).length.toString(),
+    'Content-Type': 'application/json',
+  },
+}).then(response => response.json());
